dd7e9efe572044e1564086964d5cfa7803e4af2d
Noah Kamara <mail@noahkamara.com>
2023-12-29 03:43:31 +0100

    Initial Commit

---
+++ LlamaLink/LlamaLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LlamaLinkApp.swift
+//  LlamaLink
+//
+//  Created by Noah Kamara on 29.12.23.
+//
+
+import SwiftUI
+
+@main
+struct LlamaLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
